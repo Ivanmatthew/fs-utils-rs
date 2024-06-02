7d46ab8ce0a9632c1ce8ab6eb1374592377c1984
Ivanmatthew <76107136+Ivanmatthew@users.noreply.github.com>
2024-06-03 00:46:50 +0200

    Use relevant tests

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,7 +1,25 @@
 import test from 'ava'
+import fs from 'node:fs'
 
-import { sum } from '../index.js'
+import { copyFolderRecursive, deleteFolderRecursive } from '../index.js'
 
-test('sum from native', (t) => {
-  t.is(sum(1, 2), 3)
+test.beforeEach(() => {
+  fs.mkdirSync('test', { recursive: true })
+  fs.writeFileSync('test/test.txt', 'Hello World!')
+
+  fs.mkdirSync('test/test', { recursive: true })
+  fs.writeFileSync('test/test/test.txt', 'Hello World!')
+})
+
+test('Copies succesfully, recursively.', (t) => {
+  copyFolderRecursive('test', 'test2')
+
+  t.true(fs.existsSync('test2/test.txt'))
+  t.true(fs.existsSync('test2/test/test.txt'))
+})
+
+test('Deletes succesfully, recursively.', (t) => {
+  deleteFolderRecursive('test')
+
+  t.false(fs.existsSync('test'))
 })
